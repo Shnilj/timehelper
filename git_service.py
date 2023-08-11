c10deda0ab2f0f6131d70eefdfc2ed1a23437221
Jens Descamps <jens.descamps@LM22027.local>
2023-08-11 14:33:29 +0200

    check all unmerged branches for commits + develop

--- git_service.py
+++ git_service.py
@@ -7,46 +7,46 @@ class GitService:
     with open('git_config.json') as f:
         config = json.load(f)
 
-    branch = config['branch']
     repositories = config['repositories']
+    dateRange = False
 
     def init(self):
         print("GitService init")
 
-    def set_branch(self, branch):
-        self.branch = branch
-
-        return self
-    
     def set_repositories(self, repositories):
         self.repositories = repositories
 
         return self
 
-    def get_commits_for_date(self, date, author):
+    def get_commits_for_date(self, date, authors):
         today = datetime.date.today()
         if date is None:
             date = today - datetime.timedelta(days=7)
+            self.dateRange = True
 
         commit_dict = {}
 
         for repository in self.repositories:
             repo = Repo(repository)
+            unmerged = repo.git.branch('--no-merged');
+            unmerged = ['develop'] + unmerged.split('\n')
 
-            for commit in repo.iter_commits():
-                if commit.author.name == author:
+            for branch in unmerged:
+                for commit in repo.iter_commits(branch.strip()):
                     commit_datetime = datetime.datetime.fromtimestamp(commit.authored_date)
-                    if date <= commit_datetime.date() <= today:
-                        if repository not in commit_dict:
-                            commit_dict[repository] = []
-                        commit_dict[repository].append({
-                            'hash': commit.hexsha,
-                            'message': commit.message,
-                            'time': commit_datetime.strftime('%d/%m/%Y %H:%M:%S'),
-                            'branch': commit.repo.active_branch.name
-                        })
+                    if (self.dateRange == True and date <= commit_datetime.date() <= today) or (self.dateRange == False and date == commit_datetime.date()):
+                        if commit.author.name in authors:
+                            if repository not in commit_dict:
+                                commit_dict[repository] = {}
+                            if(commit.hexsha not in commit_dict[repository]):
+                                commit_dict[repository][commit.hexsha] = {
+                                    'hash': commit.hexsha,
+                                    'message': commit.message,
+                                    'time': commit_datetime.strftime('%d/%m/%Y %H:%M:%S'),
+                                    'branch': commit.repo.active_branch.name
+                                }
 
-        for repository in commit_dict:
-            commit_dict[repository] = sorted(commit_dict[repository], key=lambda k: k['time'])
+        # for repository in commit_dict:
+        #     commit_dict[repository] = sorted(commit_dict[repository], key=lambda k: k['time'])
 
         return commit_dict
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from colorama import Fore
 from git_service import GitService
 from devops import DevOpsService
+from halo import Halo
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--date', help='The date to check in the format YYYY-MM-DD')
@@ -11,6 +12,9 @@ parser.add_argument('-t', '--tickets', help='Show the tickets that were worked o
 args = parser.parse_args()
 
 def main():
+    loader = Halo(text='Loading', spinner='dots')
+    loader.start()
+
     if args.date:
         try:
             date_to_check = datetime.datetime.strptime(args.date, '%Y-%m-%d').date()
@@ -20,13 +24,18 @@ def main():
     else:
         date_to_check = None
 
+
     if args.tickets:
-        return DevOpsService().get_recent_activity_data()
+        response = DevOpsService().get_recent_activity_data()
+        loader.stop()
+
+        return response
 
     git_service = GitService()
-    author_name = os.environ['AUTHOR_NAME'] # replace with the name you use for git commits
-    commit_dict = git_service.get_commits_for_date(date_to_check, author_name)
-    for repo, commits in commit_dict.items():
+    author_names = os.environ['AUTHOR_NAMES'] # replace with the name you use for git commits
+    commit_dict = git_service.get_commits_for_date(date_to_check, author_names)
+    for repo, commitsByHash in commit_dict.items():
+        commits = commitsByHash.values()
         print(f'Repository: {repo}')
         for commit in commits:
             print(Fore.RESET + f'Branch: {commit["branch"]}, Time: {commit["time"]}')
@@ -35,6 +44,8 @@ def main():
             print()  # print empty line
 
     print(Fore.RESET)
+
+    loader.stop()
 
 if __name__ == '__main__':
     main()
